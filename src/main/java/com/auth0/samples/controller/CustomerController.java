84ec55ae7054c32eaca70c476537703b6c4c96af
bruno.krebs <krebs.bruno@gmail.com>
2017-03-31 15:08:15 -0300

    Contacts on its own entity/table.

--- src/main/java/com/auth0/samples/controller/CustomerController.java
+++ src/main/java/com/auth0/samples/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.auth0.samples.controller;
 
+import com.auth0.samples.model.Contact;
 import com.auth0.samples.model.Customer;
 import com.auth0.samples.persistence.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,10 @@ public class CustomerController {
     @RequestMapping(path = "/customers/", method = RequestMethod.GET)
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
+    }
+
+    @RequestMapping(path = "/customers/{customerId}/contacts/", method = RequestMethod.GET)
+    public List<Contact> getCustomerContacts(@PathVariable("customerId") Long customerId) {
+        return customerRepository.findOne(customerId).getContacts();
     }
 }
